2386ba1c5b7d040ebfef30d74abea173e324cf21
njire sammy <njiresammy@gmail.com>
2025-10-03 12:01:21 +0300

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10 
+number2 = 5
+sum = number1 + number2
+diffrence = number1 - number2
+product= number1 * number2
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subraction of {number1} and {number2} is {diffrence}")
+print(f"Multiplication of {number1} and {number2} is {product}")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time 
+interest = 1000 * 0.05 * 3
+print(f"product of {principal} , {rate} and {time} is {interest}")
+print(f"The simple interest is :{interest}")
